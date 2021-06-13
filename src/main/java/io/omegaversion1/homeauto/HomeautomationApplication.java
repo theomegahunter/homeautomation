3209908b96d9285377011ccbf81f591c8b85f901
Nathan Schulke <zeus.omega@gmail.com>
2021-06-13 12:27:53 -0400

    Initial commit.

---
+++ src/main/java/io/omegaversion1/homeauto/HomeautomationApplication.java
@@ -0,0 +1,13 @@
+package io.omegaversion1.homeauto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeautomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeautomationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/omegaversion1/homeauto/HomeautomationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.omegaversion1.homeauto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeautomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
