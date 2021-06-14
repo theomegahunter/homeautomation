fa960339213a3d16d03781a1865deefee1c6ecb1
Nathan Schulke <zeus.omega@gmail.com>
2021-06-14 17:44:38 -0400

    IPDectection class outline

---
+++ src/main/java/io/omegaversion1/homeauto/IPDetection.java
@@ -0,0 +1,20 @@
+package io.omegaversion1.homeauto;
+
+
+public class IPDetection {
+
+    // Step 1
+    // Sniff the network for IP addresses/mac addresses
+    // log the results
+
+    // Step 2
+    // sniff the network for specific IP addresses/mac addresses
+    // log the results
+
+    // Step 3
+    // keep the statuses of these IPs/MACs in memory
+    // only log the results on a status change
+
+    // Step 4
+    // add the specific IP addresses /mac addresses to a kafka topic
+}
